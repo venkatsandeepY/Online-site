319e644ce03f5247c140ab59c846112bac8c236d
slk <slk@WSLKCMP2FTR1-06.SLKSOFT.COM>
2024-05-27 13:23:35 +0530

    Services

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { OnlineserviceService } from '../onlineservice.service';
 
 @Component({
   selector: 'app-buy',
   templateUrl: './buy.component.html',
-  styleUrl: './buy.component.css'
+  styleUrl: './buy.component.css',
+  providers:[OnlineserviceService]
 })
 export class BuyComponent {
+  res: string = " ";
+  constructor( public ob: OnlineserviceService) {
+    this.res = ob.add(3, 2);
+    this.res = ob.multiply(3, 2)
+  }
 
+  hi(){
+    this.ob.add(10,30);
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,30 +1,19 @@
 import { Component } from '@angular/core';
+import { OnlineserviceService, IPrd } from '../onlineservice.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
+  providers: [OnlineserviceService]
 })
 export class ProductsComponent {
-  prod: IPrd[] = [
-    { pid: "p001", pname: "TV", price: 10000, qty: 10, pimage: '/assets/tv.jpg' },
-    { pid: "p002", pname: "Washingmachine", price: 12000, qty: 9, pimage: '/assets/washingmachine.jpg' },
-    { pid: "p003", pname: "Monitor", price: 11000, qty: 3, pimage: '/assets/monitor.jpg' },
-    { pid: "p004", pname: "Watch", price: 9000, qty: 5, pimage: '/assets/watch.jpg' },
-    { pid: "p005", pname: "Fan", price: 300, qty: 9, pimage: '/assets/fan.jpg' },
-    { pid: "p006", pname: "Fridge", price: 30000, qty: 55, pimage: '/assets/Fridge.jpg' },
-    { pid: "p007", pname: "Projector", price: 80000, qty: 33, pimage: '/assets/projector.jpg' },
-    { pid: "p008", pname: "Power Bank", price: 90000, qty: 44, pimage: '/assets/Invertor.jpg' },
-    { pid: "p009", pname: "ac", price: 190000, qty: 44, pimage: '/assets/ac.jpg' },
-    { pid: "p010", pname: "car", price: 900000, qty: 44, pimage: '/assets/car.jpg' },
-    { pid: "p011", pname: "mobile", price: 19000, qty: 44, pimage: '/assets/mobile.jpg' },
-  ]
+  prod: IPrd[] = [];
+  constructor(prd: OnlineserviceService) {
+    this.prod = prd.getproducts();
+  }
   value: Boolean = true;
   showOrHide() {
     this.value = !this.value
   }
-
-}
-interface IPrd {
-  pid?:string; pname: string, price: number, qty: number, pimage: string
 }
